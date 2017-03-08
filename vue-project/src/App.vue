0edf94fc15978b8245fde43a7606014c56b04aa2
gaspar1986 <491028246@qq.com>
2017-03-08 23:49:04 +0800

    add

---
+++ vue-project/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <div class="header">i am header</div>
+    <div class="tab">i am tab</div>
+    <div class="footer">i am footer</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="less">
+  #app
+   & > .header{
+      font-size:20px;
+   }
+</style>
