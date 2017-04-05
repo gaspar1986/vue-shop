74a2147f3c4487a029bc9ea215016719ba87ae1b
gaspar1986 <491028246@qq.com>
2017-04-06 00:12:54 +0800

    login

---
+++ vue-basic/src/components/Hello.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <a href="">{{msg}}</a>
+    <button @click="show">展示</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  props:{
+      id:{
+          type:String,
+      }
+  },
+  methods:{
+      show(){
+          console.log(this.$props);
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
